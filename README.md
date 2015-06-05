7a69d7e6d78d6fe16a66adf8cd9c9b6474b10ebf
Mattias Gees <mattias.gees@gmail.com>
2015-06-05 14:15:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+puppet-sidekiq
+==============
+
+Install sidekiq
+
+## Setup
+
+### Setup Requirements
+
+None
+
+## Limitations
+
+Tested on Ubuntu 10.04, 12.04 and 14.04
---
+++ manifests/init.pp
@@ -0,0 +1,36 @@
+##### LICENSE
+
+# Copyright (c) Skyscrapers (iLibris bvba) 2014 - http://skyscrape.rs
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# == Class: sidekiq
+#
+# This class is able to install sidekiq
+#
+#
+# === Parameters
+#
+# === Examples
+#
+# * Installation of sidekiq
+#     include sidekiq
+#
+class sidekiq() {
+
+  package { 'sidekiq':
+    ensure   => present,
+    provider => 'gem',
+  }
+}
